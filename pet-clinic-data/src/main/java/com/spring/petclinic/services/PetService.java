1d26971082a6c9265d29a28226bc96ed0bf3cdcb
Matteo Galiazzo <mattegaliz@gmail.com>
2021-03-31 17:48:05 +0200

    refractoring to common crud interface

--- pet-clinic-data/src/main/java/com/spring/petclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/spring/petclinic/services/PetService.java
@@ -3,8 +3,5 @@ import com.spring.petclinic.model.Pet;
 
 import java.util.Set;
 
-public interface PetService {
-    Pet findById(Long id);
-    Pet save(Pet pet);
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long>{
 }
--- pet-clinic-data/src/main/java/com/spring/petclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/spring/petclinic/services/VetService.java
@@ -1,11 +1,7 @@
 package com.spring.petclinic.services;
-
 import com.spring.petclinic.model.Vet;
 
 import java.util.Set;
 
-public interface VetService {
-    Vet findById(Long id);
-    Vet save(Vet vet);
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long>{
 }
